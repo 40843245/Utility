44792d700dea3a72b3df700809e3a75c2b3d3841
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2023-10-04 16:02:53 +0800

    Update TextHandler.py

--- Object/String/Text/TextHandler/v1/Code/TextHandler.py
+++ Object/String/Text/TextHandler/v1/Code/TextHandler.py
@@ -4,11 +4,12 @@ class TextHandler():
     def Search(content:str,searchText):
         delimList = [ ' ','\n' , '\t' ]
         r = StringHandler.Split(content, delimList)
-        return r
+        resultList = [ (elem1,elem2,elem3) for (elem1,elem2,elem3) in r if elem3 == searchText]
+        return resultList
     @staticmethod
     def Replace( content:str, spellChecker : dict ):
         delimList = [' ','\n','\t']
-        retVal = TextHandler.Search(content,delimList)
+        retVal = StringHandler.Split(content,delimList)
         spellCheckerKeysList = list( spellChecker.keys() )        
         s = ""
         for i in range(0,len(retVal),1):
@@ -21,11 +22,19 @@ class TextHandler():
         return s
 
 if __name__ == '__main__':
-    content = 'a1 b2 c3\n d4'
+    content = "a1 b2 c3\n d4"
+    searchText = "a1"
     spellChecker = {
         'a1' : 'k1',
         'a2' : 'k2',
         'b1' : 'c2'
     }
-    r = TextHandler.Replace(content,spellChecker)
+    r = TextHandler.Search(content,searchText)
+    print('-'*20)
     print(r)
+    print('-'*20)
+    r = TextHandler.Replace(content,spellChecker)
+    print('-'*20)
+    print(r)
+    print('-'*20)
+    
